ef029b94a52e48a378819f2ce5e27fdaa318ff71
Ranjan Kumar Sharma <53993175+ranjansharma1412@users.noreply.github.com>
2023-04-10 08:55:19 +0530

    chat boat initial design

---
+++ index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+// ...
+const WebviewChatBoat = () => {
+  return <WebView source={{ uri: 'https://reactnative.dev/' }} style={{ flex: 1 }} />;
+}
