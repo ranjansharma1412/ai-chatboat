59cea27246ee289ec1ec7a7aac2c8d4158213a65
Ranjan Kumar Sharma <53993175+ranjansharma1412@users.noreply.github.com>
2023-04-10 08:58:55 +0530

    exported

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ import { WebView } from 'react-native-webview';
 const WebviewChatBoat = () => {
   return <WebView source={{ uri: 'https://reactnative.dev/' }} style={{ flex: 1 }} />;
 }
+
+export default WebviewChatBoat;
